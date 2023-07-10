4df14f7c5ef32a5cb29ede711c3b42bc1cf51a2b
Sergey Dolgikh <ageres7@gmail.com>
2023-07-10 11:40:25 +0500

    Initial Commit

---
+++ SimpleTapGame/SimpleTapGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTapGameApp.swift
+//  SimpleTapGame
+//
+//  Created by Sergey Dolgikh on 10.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTapGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
